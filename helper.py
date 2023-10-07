92b93a44d208a36831e29a1645eb3811bed78211
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-10-06 20:33:39 -0600

    Update helper.py

--- helper.py
+++ helper.py
@@ -160,59 +160,17 @@ def play_webcam(conf, model):
     """
     st.sidebar.title("Webcam Object Detection")
 
-    webrtc_streamer(
-      key="example",
-      mode = WebRtcMode.SENDRECV,
-      video_processor_factory=lambda : utils.MyVideoTransformer(conf,model),
-      rtc_configuration={"iceServers": get_ice_servers()},
-      media_stream_constraints={"video": True, "audio": False},
-      async_processing =True
-      )
-     #turn.py
-
-     import logging
-     import os
-     import streamlit as st
-     from twilio.base.exceptions import TwilioRestException
-     from twilio.rest import Client
-     logger = logging.getLogger(__name__)
-     os.environ["TWILIO_ACCOUNT_SID"] = "Twilio Account SID"
-     os.environ["TWILIO_AUTH_TOKEN"] = "Twilio Auth"
-     def get_ice_servers():
-"""Use Twilio's TURN server because Streamlit Community Cloud has changed
-its infrastructure and WebRTC connection cannot be established without TURN server now. # noqa: E501
-We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
-but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656 # noqa: E501
-See https://github.com/whitphx/streamlit-webrtc/issues/1213
-"""
-   # Ref: https://www.twilio.com/docs/stun-turn/api
-    try:
-     account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-     auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-    except KeyError:
-    logger.warning(
-   "Twilio credentials are not set. Fallback to a free STUN server from Google." # noqa: E501
+  ctx =  webrtc_streamer(
+        key="example",
+        video_transformer_factory=VideoTransformer,
+        client_settings=WEBRTC_CLIENT_SETTINGS,)
+      # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+      # media_stream_constraints={"video": True, "audio": False},
     )
-return [{"urls": ["stun:stun.l.google.com:19302"]}]
-
-client = Client(account_sid, auth_token)
-
-try:
-
-token = client.tokens.create()
-
-except TwilioRestException as e:
-
-st.warning(
-
-f"Error occurred while accessing Twilio API. Fallback to a free STUN server from Google. ({e})" # noqa: E501
-
-)
-
-return [{"urls": ["stun:stun.l.google.com:19302"]}]
-
-return token.ice_servers
-   
+        if ctx.video_transformer:
+        ctx.video_transformer.model = model
+        ctx.video_transformer.rgb_colors = rgb_colors
+        ctx.video_transformer.target_class_ids = target_class_ids
 
 class MyVideoTransformer(VideoTransformerBase):
     def __init__(self, conf, model):
@@ -223,7 +181,6 @@ class MyVideoTransformer(VideoTransformerBase):
         image = frame.to_ndarray(format="bgr24")
         processed_image = self._display_detected_frames(image)
         st.image(processed_image, caption='Detected Video', channels="BGR", use_column_width=True)
-        return av.VideoFrame.from_ndarray(processed_image, format="bgr24")
 
     def _display_detected_frames(self, image):
         orig_h, orig_w = image.shape[0:2]
