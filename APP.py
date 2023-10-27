42313022a06d27df64e9f07ff237b5b9813f2eea
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-10-27 03:57:11 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -27,7 +27,7 @@ st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
 # Model Options
 model_type = st.sidebar.radio(
-    "Seleccionar tarea", ['Detectar', 'Segmentar'])
+    "Seleccionar tarea", ['Detection', 'Segmentation'])
 
 confidence = float(st.sidebar.slider(
     "Select Model Confidence", 25, 100, 40)) / 100
