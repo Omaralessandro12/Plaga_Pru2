e3f523462852b6c821f82e1a54a4cffae2b43e02
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-10-27 04:23:47 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -53,7 +53,7 @@ source_img = None
 # If image is selected
 if source_radio == settings.IMAGE:
     source_img = st.sidebar.file_uploader(
-        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+        "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
     col1, col2 = st.columns(2)
 
