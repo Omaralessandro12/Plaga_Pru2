cf9d30d2e14448bd65361833ff5340ca22c753ce
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-05-19 08:43:33 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -15,25 +15,19 @@ if root_path not in sys.path:
 ROOT = root_path.relative_to(Path.cwd())
 
 # Sources
-#IMAGE = 'Imagen'
+IMAGE = 'Imagen'
 #VIDEO = 'Video'
 #WEBCAM = 'Camara'
 #YOUTUBE = 'YouTube'
 
 SOURCES_LIST = [IMAGE]
 
-# Images config
-IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
 # ML Model config
 MODEL_DIR = ROOT / 'weights'
 DETECTION_MODEL = MODEL_DIR / 'best.pt'
 SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
 
-# Webcam
-WEBCAM_PATH = 0
 
 # Establecer la fuente predeterminada como 'Imagen'
 source_radio = 'Imagen'
